802e336aa52512500ee3881711446d5ee8e3c3df
Michał Witek <michal.witek.katowice@gmail.com>
2023-01-16 18:43:47 +0100

    add init preloader

--- pagination/index.js
+++ pagination/index.js
@@ -1,65 +1,84 @@
 console.log('Pagination');
 
-fetchJsonResponse('http://swapi.dev/api/people')
-    .then(response => {
-        renderPeoplePaginator(response['count']);
-        renderPeopleList(response['results']);
-    });
+window.onload = () => {
+    const loadButton = document.getElementById('load-people-button');
+    const preloader = document.getElementById('loading-container');
 
-function fetchJsonResponse(url){
-    return fetch(url)
-        .then(response => response.json())
-        .catch(error => console.log('Error: ', error))
-}
+    loadButton.addEventListener('click', () => {
 
-function renderPeoplePaginator(count, pageSize = 10, selectedPage = 1) {
-    const pages = Math.ceil(count / pageSize);
-    const select = document.getElementById('people-pagination');
-    select.innerHTML = '';
+        loadButton.classList.add('hidden');
+        preloader.classList.remove('hidden');
 
-    for(let i =1; i <= pages; i++){
-        const option = document.createElement('option');
-        option.innerText = i;
-        option.value = i;
-        if(i === Number(selectedPage)){
-            option.setAttribute('selected', true)
-        }
-        select.append(option);
-    }
-    select.addEventListener('change', (event) => {
-        fetchJsonResponse(`http://swapi.dev/api/people/?page=${event.target.value}`)
+        fetchJsonResponse('http://swapi.dev/api/people')
             .then(response => {
-                renderPeoplePaginator(response['count'], 10, event.target.value);
+                renderPeoplePaginator(response['count']);
                 renderPeopleList(response['results']);
-            });
+            }).then(() => {
+            loadButton.classList.add('hidden');
+            preloader.classList.add('hidden');
+
+            document.getElementById('pagination-container').classList.remove('hidden');
+            document.getElementById('people-container').classList.remove('hidden');
+        })
     });
-}
 
-function renderPeopleList(people) {
-    const peopleHtmlElems = people.map((person, index) => getPersonLayout(person, index))
-    const peopleList = document.querySelector('#people-container');
-    peopleList.innerHTML = '';
-    peopleHtmlElems.forEach(elem => peopleList.append(elem));
-}
-
-function getPersonLayout(person, index) {
-    const root = document.createElement('div');
-    root.classList.add('container-item');
-
-    root.append(getPersonProp('', person['name']));
-    root.append(getPersonProp('Birth Year: ', person['birth_year'], index));
-    root.append(getPersonProp('Skin: ', person['skin_color'], index));
-    root.append(getPersonProp('Hair: ', person['hair_color'], index));
-
-    return root;
-}
-
-function getPersonProp(title, property, index) {
-    const prop = document.createElement('div');
-    prop.classList.add('card-title');
-    if (index !== undefined) {
-        prop.classList.add(`person-toggle-prop-${index}`);
+    function fetchJsonResponse(url) {
+        return fetch(url)
+            .then(response => response.json())
+            .catch(error => console.log('Error: ', error))
     }
-    prop.innerText = `${title}${property}`;
-    return prop;
+
+    function renderPeoplePaginator(count, pageSize = 10, selectedPage = 1) {
+        const pages = Math.ceil(count / pageSize);
+        const select = document.getElementById('people-pagination');
+        select.innerHTML = '';
+
+        for (let i = 1; i <= pages; i++) {
+            const option = document.createElement('option');
+            option.innerText = i;
+            option.value = i;
+            if (i === Number(selectedPage)) {
+                option.setAttribute('selected', true)
+            }
+            select.append(option);
+        }
+        select.addEventListener('change', (event) => {
+
+
+            fetchJsonResponse(`http://swapi.dev/api/people/?page=${event.target.value}`)
+                .then(response => {
+                    renderPeoplePaginator(response['count'], 10, event.target.value);
+                    renderPeopleList(response['results']);
+                });
+        });
+    }
+
+    function renderPeopleList(people) {
+        const peopleHtmlElems = people.map((person, index) => getPersonLayout(person, index))
+        const peopleList = document.querySelector('#people-container');
+        peopleList.innerHTML = '';
+        peopleHtmlElems.forEach(elem => peopleList.append(elem));
+    }
+
+    function getPersonLayout(person, index) {
+        const root = document.createElement('div');
+        root.classList.add('container-item');
+
+        root.append(getPersonProp('', person['name']));
+        root.append(getPersonProp('Birth Year: ', person['birth_year'], index));
+        root.append(getPersonProp('Skin: ', person['skin_color'], index));
+        root.append(getPersonProp('Hair: ', person['hair_color'], index));
+
+        return root;
+    }
+
+    function getPersonProp(title, property, index) {
+        const prop = document.createElement('div');
+        prop.classList.add('card-title');
+        if (index !== undefined) {
+            prop.classList.add(`person-toggle-prop-${index}`);
+        }
+        prop.innerText = `${title}${property}`;
+        return prop;
+    }
 }
