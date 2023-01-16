52785c51985209c6d192e6609f8c6928a68c3c9a
Michał Witek <michal.witek.katowice@gmail.com>
2023-01-16 21:54:48 +0100

    update after classes

--- pagination/index.js
+++ pagination/index.js
@@ -14,7 +14,6 @@ window.onload = () => {
                 renderPeoplePaginator(response['count']);
                 renderPeopleList(response['results']);
             }).then(() => {
-            loadButton.classList.add('hidden');
             preloader.classList.add('hidden');
 
             document.getElementById('pagination-container').classList.remove('hidden');
@@ -28,14 +27,15 @@ window.onload = () => {
             .catch(error => console.log('Error: ', error))
     }
 
-    function renderPeoplePaginator(count, pageSize = 10, selectedPage = 1) {
+    function renderPeoplePaginator(count, selectedPage = 1, pageSize = 10) {
         const pages = Math.ceil(count / pageSize);
+
         const select = document.getElementById('people-pagination');
         select.innerHTML = '';
 
         for (let i = 1; i <= pages; i++) {
             const option = document.createElement('option');
-            option.innerText = i;
+            option.innerText = 'Page ' + i;
             option.value = i;
             if (i === Number(selectedPage)) {
                 option.setAttribute('selected', true)
@@ -43,41 +43,51 @@ window.onload = () => {
             select.append(option);
         }
         select.addEventListener('change', (event) => {
+            const selectedPage = event.target.value;
+            const preloader = document.getElementById('loading-container');
+            const paginatorElem = document.getElementById('pagination-container');
+            const peopleList = document.getElementById('people-container');
 
+            preloader.classList.remove('hidden');
+            paginatorElem.classList.add('hidden');
+            peopleList.classList.add('hidden');
 
-            fetchJsonResponse(`http://swapi.dev/api/people/?page=${event.target.value}`)
+            fetchJsonResponse(`http://swapi.dev/api/people/?page=${selectedPage}`)
                 .then(response => {
-                    renderPeoplePaginator(response['count'], 10, event.target.value);
+                    renderPeoplePaginator(response['count'], selectedPage);
                     renderPeopleList(response['results']);
-                });
+                }).then(() => {
+                setTimeout(() => {
+                    preloader.classList.add('hidden');
+                    paginatorElem.classList.remove('hidden');
+                    peopleList.classList.remove('hidden');
+                }, 1500);
+            });
         });
     }
 
     function renderPeopleList(people) {
-        const peopleHtmlElems = people.map((person, index) => getPersonLayout(person, index))
+        const peopleHtmlElems = people.map(person => getPersonLayout(person))
         const peopleList = document.querySelector('#people-container');
         peopleList.innerHTML = '';
         peopleHtmlElems.forEach(elem => peopleList.append(elem));
     }
 
-    function getPersonLayout(person, index) {
+    function getPersonLayout(person) {
         const root = document.createElement('div');
         root.classList.add('container-item');
 
         root.append(getPersonProp('', person['name']));
-        root.append(getPersonProp('Birth Year: ', person['birth_year'], index));
-        root.append(getPersonProp('Skin: ', person['skin_color'], index));
-        root.append(getPersonProp('Hair: ', person['hair_color'], index));
+        root.append(getPersonProp('Birth Year: ', person['birth_year']));
+        root.append(getPersonProp('Skin: ', person['skin_color']));
+        root.append(getPersonProp('Hair: ', person['hair_color']));
 
         return root;
     }
 
-    function getPersonProp(title, property, index) {
+    function getPersonProp(title, property) {
         const prop = document.createElement('div');
         prop.classList.add('card-title');
-        if (index !== undefined) {
-            prop.classList.add(`person-toggle-prop-${index}`);
-        }
         prop.innerText = `${title}${property}`;
         return prop;
     }
