b8e1d943b09dbe85141eecc2c1cf4c492004c75d
Michał Witek <michal.witek.katowice@gmail.com>
2023-04-20 14:43:05 +0200

    add lazy load on scroll example

--- throttle-debounce-lazyload/index.js
+++ throttle-debounce-lazyload/index.js
@@ -1,19 +1,19 @@
 console.log('Throttle, debounce and lazy load');
 
-function updateView(clientX, clientY, mouseMoveCount) {
-    const mouseMoveCounterEl = document.querySelector('#mouseMoveCount');
-    const mousePositionXEl = document.querySelector('#mousePositionX');
-    const mousePositionYEl = document.querySelector('#mousePositionY');
-
-    mouseMoveCounterEl.innerText = mouseMoveCount;
-    mousePositionXEl.innerText = clientX;
-    mousePositionYEl.innerText = clientY
-}
-
-function updateTimeCounter(value){
-    const mouseWithTimeControlCounterEl = document.querySelector('#mouseWithTimeControlCounter');
-    mouseWithTimeControlCounterEl.innerText = value;
-}
+// function updateView(clientX, clientY, mouseMoveCount) {
+//     const mouseMoveCounterEl = document.querySelector('#mouseMoveCount');
+//     const mousePositionXEl = document.querySelector('#mousePositionX');
+//     const mousePositionYEl = document.querySelector('#mousePositionY');
+//
+//     mouseMoveCounterEl.innerText = mouseMoveCount;
+//     mousePositionXEl.innerText = clientX;
+//     mousePositionYEl.innerText = clientY
+// }
+//
+// function updateTimeCounter(value){
+//     const mouseWithTimeControlCounterEl = document.querySelector('#mouseWithTimeControlCounter');
+//     mouseWithTimeControlCounterEl.innerText = value;
+// }
 
 //Throttle and debounce with lodash
 // window.onload = () => {
@@ -63,4 +63,31 @@ function updateTimeCounter(value){
 //         mouseWithTimeControlCount++;
 //         updateTimeCounter(mouseWithTimeControlCount);
 //     });
+// }
+
+//Lazy load
+// window.onload = () => {
+//     //produce some sample data
+//     const data = _.range(1, 100).map(item => ({id: item, value: Math.round(Math.random() * 1000)}));
+//     const pageSize = 20;
+//     let loadIndex = 0;
+//
+//     //init table with first 20 rows;
+//     addRowsToTable(_.slice(data, loadIndex, loadIndex + pageSize));
+//
+//     //load more elems when user reaches the bottom of the page
+//     document.addEventListener('scroll', event => {
+//         if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+//             if (data.length > loadIndex + pageSize) {
+//                 loadIndex = loadIndex + pageSize;
+//                 addRowsToTable(_.slice(data, loadIndex, loadIndex + pageSize));
+//             }
+//         }
+//     });
+//
+//     function addRowsToTable(data) {
+//         const tableEl = document.querySelector('#data-table');
+//         const rows = data.map(item => `<tr class="table-row"><td class="table-td-id">${item.id}</td><td>${item.value}</td></tr>`);
+//         tableEl.innerHTML = tableEl.innerHTML + rows.join('');
+//     }
 // }
